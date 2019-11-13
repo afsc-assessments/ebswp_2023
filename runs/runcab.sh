7fb5eebfc6560f23e6c49611c9c654937afa3c8c
jimianelli <jim.ianelli@gmail.com>
2019-11-13 10:19:56 -0800

    dir

---
+++ runs/runcab.sh
@@ -0,0 +1,25 @@
+cd vast_cp
+make
+cd ..
+cd vast_cp_ats
+make
+cd ..
+cd vast_nocp
+make
+cd ..
+cd C
+make
+cd ..
+cd CA
+make
+cd ..
+cd CAB
+make
+cd ..
+cd CABA
+make
+cd ..
+
+cd q_sens
+prof.sh
+cd ..
