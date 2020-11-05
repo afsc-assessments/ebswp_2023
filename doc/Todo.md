2db91527b1536d7b8e3aba278cc6434b0cccef7c
jimianelli <jim.ianelli@gmail.com>
2020-11-05 07:03:26 -0800

    Added missing files

---
+++ doc/Todo.md
@@ -0,0 +1,7 @@
+# To do list
+    [ ] Get CPUE plots
+    [ ] Get Tier SRR plot
+    [ ] N wt length and ages fisheries 2019
+    [ ] Nyrs avg sel 
+    [ ] review SAFE Guidelines, include Grant's SIS F
+    [ ] review SAFE Guidelines, include Grant's SIS F
---
+++ pm_20.R
@@ -0,0 +1,66 @@
+setwd("~/_mymods/ebswp/doc")
+library(doParallel)
+library(patchwork)
+registerDoParallel(5)
+source("../R/prelims.R")
+thisyr    = 2020
+lastyr    = thisyr-1
+nextyr    = thisyr+1
+
+# The model specs
+.THEME    = theme_few(base_size = 11, base_family = "")
+.OVERLAY  = TRUE
+
+# Read report file and create gmacs report object (a list):
+#mod_names <- c("No AVO","Model 16.1","VAST","Low AVO", "High AVO")
+#mod_names <- c("2020","last year","Low AVO")
+mod_names <- c("2020","last year","with saildrone","conditioned SRR")
+#.MODELDIR = c( "../runs/2020/","../runs/last_year/","../runs/2020Lavo/")
+#.MODELDIR = c( "../runs/2020/","../runs/last_year/","../runs/sr1/")
+.MODELDIR = c( "../runs/2020/","../runs/last_year/","../runs/base/","../runs/sr1/")
+fn       <- paste0(.MODELDIR, "pm")
+
+system.time( modlst <- mclapply(fn, read_admb,mc.cores=4) )
+
+thismod <- 3 # the selected model
+names(modlst) <- mod_names
+# The model picked
+nmods <- length(mod_names)
+#add_stuff<-function(idx){ proj_file<- paste0(.MODELDIR[idx],"proj/bigfile.out") #modlst[[idx]] <- c(modlst[[idx]],get_vars(modlst[[idx]])) return( c(modlst[[i]],get_vars(modlst[[i]])) ) }
+#system.time( modlst<-mclapply(1:nmods,add_stuff,mc.cores=nmods) )
+#system.time( 
+i=1
+for (i in 1:nmods) {
+  proj_file<- paste0(.MODELDIR[i],"proj/bigfile.out")
+  modlst[[i]] <- c(modlst[[i]],get_vars(modlst[[i]])) 
+} 
+ # )
+##names(M)
+
+#proj_file<- paste0(.MODELDIR[thismod],"proj/bigfile.out")
+#t2 <- read.table(proj_file,header=TRUE)
+#names(modlst)
+M        <- modlst[[thismod]]
+P        <- modlst[[2]] # Last year's model (P=previous)
+Alt      <- modlst[[1]] # Last year's model (P=previous)
+
+#
+rhodf      <- read.csv("../doc/data/mohnrho.csv",header=T)
+rhoMohn10  <-  rhodf[11,3]
+rhoMohn20  <-  rhodf[21,3]
+
+fc  <- (read_csv("../doc/data/fig_captions.csv"))
+figcap <<- fc$cap; figlab <<- fc$label; fnum <<- fc$no
+reffig <<- function(i){ cat(paste0("\\ref{fig:",figlab[fnum==i],"}")) }  
+printfig <<- function(tmp,i){ cat(paste0("\n![",figcap[fnum==i],"\\label{fig:",figlab[fnum==i],"}](figs/",tmp,")   \n ")) } 
+
+tc  <- (read_csv("../doc/data/table_captions.csv"))
+tabcap <- tc$cap; tablab <- tc$label
+# tap <- data_frame(t=c(1,2),c=c(1,2))
+printtab <<- function(tmp,i){tab <- xtable(tmp, digits=0, auto=TRUE,caption = tabcap[i], label = paste0("tab:",tablab[i])); print(tab, caption.placement = "top", include.rownames = FALSE, sanitize.text.function = function(x){x}) } 
+reftab <<- function(i){ cat(paste0("tab:",tablab[i])) }  
+#print(tablab)
+
+#source("../R/Do_Plots.R")
+#source("../R/Do_MCMC.R")
+#source("../R/Do_Proj.R")
