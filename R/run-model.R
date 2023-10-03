5151b768315c29a99b649697a4af3075c532edc9
Jim Ianelli <jim.ianelli@gmail.com>
2023-10-03 06:12:32 -0700

    Added base run directory to run_model function

--- R/run-model.R
+++ R/run-model.R
@@ -4,7 +4,7 @@
 #' @export
 #'
 .run_mod <- function(moddir=mod_dir){
-  rn        <- paste0("cd runs/",moddir,"; pm -nox -iprint 500")
+  rn        <- paste0("cd ",moddir,"; pm -nox -iprint 500")
 	system(rn)
 }
 #' Run Multiple ADMB Models in Parallel
@@ -13,13 +13,14 @@
 #' gather their outputs. After running the models, it also fetches certain variables
 #' from a 'proj/spm_detail.csv' file associated with each model.
 #'
+#' @param rundir Character string. The base run directory path for the models, default is `runs`.
 #' @param moddir Character vector. The directory paths for the models, default is `mod_dir`.
 #' @param modnames Character vector. The names of the models, default is `mod_names`.
 #' @param Output Logical. A flag indicating if any output should be displayed (e.g., messages,
 #'   progress, etc.), default is `FALSE`.
 #' @return A list containing the outputs of the ADMB models and the fetched variables.
 #' @export
-run_model <- function(moddir=mod_dir, modnames=mod_names,Output=TRUE){
+run_model <- function(moddir=mod_dir, rundir="runs",modnames=mod_names,Output=TRUE){
   modlst <- NULL
   nmods <- length(mod_dir)
   num_cores <- min(nmods,parallel::detectCores() - 1)
@@ -29,6 +30,7 @@ run_model <- function(moddir=mod_dir, modnames=mod_names,Output=TRUE){
   # Export necessary functions and objects to the cluster
   parallel::clusterExport(cl, c(".run_mod","read_fit", "read_admb", "read_rep"), envir=environment())
 
+	moddir <- paste0(rundir,"/",moddir)
   # Fetch model results in parallel
   system.time(modlst <- parallel::parLapply(cl=cl, X=moddir, fun=.run_mod))
   if (Output)
--- README.md
+++ README.md
@@ -88,3 +88,25 @@ together and published on the website
 
 Developers will want to do things slightly differently. See the
 `Model development` vignette.
+
+# Acronymns
+
+NOAA: National Oceanic and Atmospheric Administration  
+NMFS: National Marine Fisheries Service  
+AFSC: Alaska Fisheries Science Center  
+REFM: Resource and Ecology and Fisheries Management
+
+# Legal disclaimer
+
+This repository is a software product and is not official communication
+of the National Oceanic and Atmospheric Administration (NOAA), or the
+United States Department of Commerce (DOC). All NOAA GitHub project code
+is provided on an ‘as is’ basis and the user assumes responsibility for
+its use. Any claims against the DOC or DOC bureaus stemming from the use
+of this GitHub project will be governed by all applicable Federal law.
+Any reference to specific commercial products, processes, or services by
+service mark, trademark, manufacturer, or otherwise, does not constitute
+or imply their endorsement, recommendation, or favoring by the DOC. The
+DOC seal and logo, or the seal and logo of a DOC bureau, shall not be
+used in any manner to imply endorsement of any commercial product or
+activity by the DOC or the United States Government.
