990cca35e3589f08123617d2d8ce471fa7c2138c
careymcg <carey.mcgilliard@gmail.com>
2024-06-27 16:39:06 -0700

    Added windows functionality to main run code in parallel
    added pm_windows.R and .run_mod_windows and .run_proj_windows. Added run_on_mac options to run_model and run_proj functions. Used source instead of src as main folder.

---
+++ R/ebswp-package.R
@@ -0,0 +1,7 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stringr str_split_i
+## usethis namespace: end
+NULL
--- R/run-proj.R
+++ R/run-proj.R
@@ -7,6 +7,26 @@
   rn        <- paste0("cd ",moddir,"/proj; spm; ")
 	system(rn)
 }
+
+#' run projection model for windows
+#'
+#' @param moddir directory where pollock model should be run
+#' @export
+#'
+.run_proj_windows <- function(moddir=mod_dir){
+  #make a batch file and run it
+  #parallel function below only takes one arg from .run_mod_windows, so runs folder is hardwired here- not ideal
+  #there is some way to make a dataframe or list input that Carey did not get working yet
+  #run_name<-stringr::str_split_i(moddir, fixed("\\"),2) #this was an attempt to enter the runs folder and mod_dir in the same argument. It worked for a single run, but not within the parallel processing
+  #my_file<-paste0("run_pm_carey_",run_name,".bat")
+  my_file<-paste0("run_proj_carey_",moddir,".bat")
+  write(paste0("cd ","C:\\GitProjects\\EBSpollock\\2023_runs\\",moddir,"\\proj "),my_file)
+  #write(paste0("cd ","C:\\GitProjects\\EBSpollock\\",moddir," "),my_file)
+
+  write(paste0("spm"),my_file,append = TRUE)
+  system(my_file)
+  file.remove(my_file)
+}
 #' Run Multiple projection models in Parallel
 #'
 #' This function uses parallel processing to run multiple ADMB models and
@@ -18,15 +38,27 @@
 #' @param modnames Character vector. The names of the models, default is `mod_names`.
 #'   progress, etc.), default is `FALSE`.
 #' @export
-run_proj <- function(moddir=mod_dir, rundir="runs",modnames=mod_names){
+run_proj <- function(moddir=mod_dir, rundir="runs",modnames=mod_names,run_on_mac=TRUE) {
   modlst <- NULL
   nmods <- length(mod_dir)
   num_cores <- min(nmods,parallel::detectCores() - 1)
   cl <- parallel::makeCluster(num_cores)
   on.exit(parallel::stopCluster(cl))  # Ensure the cluster stops after function execution
-	moddir <- paste0(rundir,"/",moddir)
 
+  if (run_on_mac) {
+	moddir <- paste0(rundir,"/",moddir)
   # Export necessary functions and objects to the cluster
   parallel::clusterExport(cl, c(".run_proj","read_fit", "read_admb", "read_rep"), envir=environment())
-
+  system.time(modlst <- parallel::parLapply(cl=cl, X=moddir, fun=.run_proj))
+    } else {
+    #run on windows
+      #run on windows
+      parallel::clusterExport(cl, c(".run_proj_windows","read_fit", "read_admb", "read_rep"), envir=environment())
+      #moddir <- paste0(rundir,"\\",moddir)
+      # Fetch model results in parallel
+      system.time(modlst <- parallel::parLapply(cl=cl, X=moddir, fun=.run_proj_windows))
+  }
 }
+
+
+
