8c738b09e736311f883f509301785e69dd9d9081
careymcg <carey.mcgilliard@gmail.com>
2024-07-03 14:26:26 -0700

    small update to plot-recruitment.R
    gets rid of unused "limits" argument in geom_bar

--- R/plot-recruitment.R
+++ R/plot-recruitment.R
@@ -1,11 +1,11 @@
 #' Get recruitment data
-#' 
+#'
 #' Extracts predicted recruitment and approximate asymptotic error-bars
 #'
 #' @param M list object(s) created by read_admb function
 #' @return dataframe of recruitment
 #' @author SJD Martell, DN Webber
-#' 
+#'
 .get_recruitment_df <- function(M)
 {
   #M=scset
@@ -34,7 +34,7 @@
 #' @return Plot of predicted recruitment
 #' @author SJD Martell, DN Webber
 #' @export
-#' 
+#'
 plot_recruitment <- function(M, xlab = "Year", ylab = "Recruitment (millions of individuals)",xlim=c(1990.5,2019.5),fill="yellow",alpha=.9) {
     xlab <- paste0("\n", xlab)
     ylab <- paste0(ylab, "\n")
@@ -42,13 +42,14 @@ plot_recruitment <- function(M, xlab = "Year", ylab = "Recruitment (millions of 
     if (length(M) == 1)
     {
         p <- ggplot(mdf, aes(x = year, y = rec)) +
-            geom_bar(stat = "identity", alpha = 0.4, position = "dodge", col="grey60", fill=fill) + 
+            geom_bar(stat = "identity", alpha = 0.4, position = "dodge", col="grey60", fill=fill) +
             scale_x_continuous(limits=xlim, breaks = seq(round(xlim[1],0),round(xlim[2],0), by = 5) ) +
             geom_pointrange(aes(ymax = ub, ymin = lb),size=.3 ) #, position = position_dodge(width = 1.9))
     } else {
         p <- ggplot(mdf, aes(x = year, y = rec, col = Model, group = Model)) +
             scale_x_continuous(limits=xlim, breaks = seq(round(xlim[1],0),round(xlim[2],0), by = 2) ) +
-            geom_bar(stat = "identity", alpha = alpha, aes(fill = Model), limits=xlim, position = "dodge") + 
+#            geom_bar(stat = "identity", alpha = alpha, aes(fill = Model), limits=xlim, position = "dodge") + #Carey commented this out and added the line below
+          geom_bar(stat = "identity", alpha = alpha, aes(fill = Model), position = "dodge") +
             geom_pointrange(aes(year, rec, ymax = ub, ymin = lb),col = "grey",  size=.6,position = position_dodge(width = 0.9))
     }
     p <- p + labs(x = xlab, y = ylab) + .THEME
